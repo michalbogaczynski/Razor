c5fbd073d4b2c120788e7543f6e9acde48abdc8e
michalbogaczynski <bogaczynski.m@gmail.com>
2019-11-26 21:35:49 +0100

    Praca z silnkiem Razor

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Kajak",
+            Description = "Jednoosobowa łódka",
+            Category = "Sporty wodne",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+    }
+}
